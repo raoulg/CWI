09db2b52e908b81041ceb5f7b999c1e8bf17f635
Raoul Grouls <raoul@evoluminate.nl>
2017-10-15 23:30:17 +0200

    python tests

--- pythontests/arangeseq.py
+++ pythontests/arangeseq.py
@@ -1,7 +1,9 @@
 import numpy as np
 list = np.arange(0.5, 1.3, 0.1) # snelheden tussen 0.5 en 1.2, in stappen van 0.1
 xvelWiden = np.repeat(list, 4) # repeat voor 4 objecten
-x = xvelWiden[::-1]
-xvelList = np.ndarray.tolist(xvelWiden)
-
-print(x)
+apnd = xvelWiden[0]
+print(xvelWiden)
+print(apnd)
+#x1 = xvelWiden[::-1]
+#x = np.append(x1, apnd)
+#print(x)
